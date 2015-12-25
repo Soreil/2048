f6b71414b30729feb4450910ce1e15f893021103
Sjon Kosse <sjon@x61.eu>
2015-12-25 02:16:47 +0100

    Most of the framework in place. Things that need need to be added: Help dialog for the help button Gameplay logic Animations and dynamic tile sizes Ability to add different tile sets

--- main.go
+++ main.go
@@ -1,22 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
-	"sync"
 
+	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
 )
 
+const (
+	keyLeft  = 65361
+	keyDown  = 65364
+	keyUp    = 65362
+	keyRight = 65363
+	keyH     = 104
+	keyJ     = 106
+	keyK     = 107
+	keyL     = 108
+)
+
+type tile struct {
+	event *gtk.EventBox
+	image *gtk.Image
+}
+
 type app struct {
-	window       *gtk.Window
-	resetButton  *gtk.Button
-	aboutButton  *gtk.Button
-	scoreCounter *gtk.Label
-	images       [16]*gtk.Image
-	statusBar    *gtk.Statusbar
-	sync.Mutex
+	window        *gtk.Window
+	about         *gtk.AboutDialog
+	headerBar     *gtk.HeaderBar
+	resetButton   *gtk.Button
+	helpButton    *gtk.Button
+	aboutButton   *gtk.Button
+	encouragement *gtk.Label
+	scoreLabel    *gtk.Label
+	scoreCounter  *gtk.Label
+	tiles         [16]tile
+	statusBar     *gtk.Statusbar
+	statusID      uint
+}
+
+var nums = map[int]string{
+	2:    "img/2.png",
+	4:    "img/4.png",
+	8:    "img/8.png",
+	16:   "img/16.png",
+	32:   "img/32.png",
+	64:   "img/64.png",
+	128:  "img/128.png",
+	256:  "img/256.png",
+	512:  "img/512.png",
+	1024: "img/1024.png",
+	2048: "img/2048.png",
+	4096: "img/4096.png",
 }
 
 func main() {
@@ -33,8 +69,25 @@ func main() {
 	builder.AddFromFile("ui.glade")
 
 	var app app
+	for i := 0; i < 16; i++ {
+		n := strconv.Itoa(i + 1)
+		obj, err := builder.GetObject("eventTile" + n)
+		if err != nil {
+			panic(err)
+		}
 
-	obj, err := builder.GetObject("button1")
+		if b, ok := obj.(*gtk.EventBox); ok {
+			app.tiles[i].event = b
+		}
+		obj, err = builder.GetObject("imageTile" + n)
+		if err != nil {
+			panic(err)
+		}
+		if b, ok := obj.(*gtk.Image); ok {
+			app.tiles[i].image = b
+		}
+	}
+	obj, err := builder.GetObject("resetButton")
 	if err != nil {
 		panic(err)
 	}
@@ -42,7 +95,15 @@ func main() {
 		app.resetButton = b
 	}
 
-	obj, err = builder.GetObject("button2")
+	obj, err = builder.GetObject("helpButton")
+	if err != nil {
+		panic(err)
+	}
+	if b, ok := obj.(*gtk.Button); ok {
+		app.helpButton = b
+	}
+
+	obj, err = builder.GetObject("aboutButton")
 	if err != nil {
 		panic(err)
 	}
@@ -50,7 +111,23 @@ func main() {
 		app.aboutButton = b
 	}
 
-	obj, err = builder.GetObject("label3")
+	obj, err = builder.GetObject("encouragement")
+	if err != nil {
+		panic(err)
+	}
+	if b, ok := obj.(*gtk.Label); ok {
+		app.encouragement = b
+	}
+
+	obj, err = builder.GetObject("scoreLabel")
+	if err != nil {
+		panic(err)
+	}
+	if b, ok := obj.(*gtk.Label); ok {
+		app.scoreLabel = b
+	}
+
+	obj, err = builder.GetObject("scoreCounter")
 	if err != nil {
 		panic(err)
 	}
@@ -58,7 +135,31 @@ func main() {
 		app.scoreCounter = b
 	}
 
-	obj, err = builder.GetObject("window1")
+	obj, err = builder.GetObject("statusBar")
+	if err != nil {
+		panic(err)
+	}
+	if w, ok := obj.(*gtk.Statusbar); ok {
+		app.statusBar = w
+	}
+
+	obj, err = builder.GetObject("headerBar")
+	if err != nil {
+		panic(err)
+	}
+	if w, ok := obj.(*gtk.HeaderBar); ok {
+		app.headerBar = w
+	}
+
+	obj, err = builder.GetObject("about")
+	if err != nil {
+		panic(err)
+	}
+	if w, ok := obj.(*gtk.AboutDialog); ok {
+		app.about = w
+	}
+
+	obj, err = builder.GetObject("window")
 	if err != nil {
 		panic(err)
 	}
@@ -66,7 +167,7 @@ func main() {
 		app.window = w
 	}
 
-	signalmap["aboutClicked"] = func() {
+	signalmap["helpCLicked"] = func() {
 		s, err := app.scoreCounter.GetText()
 		if err != nil {
 			panic(err)
@@ -83,15 +184,67 @@ func main() {
 		app.scoreCounter.SetText(ns)
 	}
 
+	signalmap["aboutClicked"] = func() {
+		r := gtk.ResponseType(app.about.Run())
+		if r == gtk.RESPONSE_DELETE_EVENT || r == gtk.RESPONSE_CANCEL {
+			app.about.Hide()
+		}
+	}
+
 	signalmap["resetClicked"] = func() {
-		fmt.Println("Reset clicked!")
+		for i := 0; i < 16; i++ {
+			app.tiles[i].image.SetFromFile("img/empty.png")
+		}
+		app.scoreCounter.SetLabel("0")
 	}
 
 	signalmap["removeWindow"] = func() {
 		gtk.MainQuit()
 	}
+
+	signalmap["inputHandler"] = func(win *gtk.Window, ev *gdk.Event) {
+		keyEvent := &gdk.EventKey{ev}
+		switch keyEvent.KeyVal() {
+		case keyLeft:
+			app.statusBar.Push(app.statusID, "left pressed")
+			app.tiles[0+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyH:
+			app.statusBar.Push(app.statusID, "left pressed")
+			app.tiles[0+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyDown:
+			app.statusBar.Push(app.statusID, "down pressed")
+			app.tiles[1+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyJ:
+			app.statusBar.Push(app.statusID, "down pressed")
+			app.tiles[1+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyUp:
+			app.statusBar.Push(app.statusID, "up pressed")
+			app.tiles[2+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyK:
+			app.statusBar.Push(app.statusID, "up pressed")
+			app.tiles[2+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyRight:
+			app.statusBar.Push(app.statusID, "right pressed")
+			app.tiles[3+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		case keyL:
+			app.statusBar.Push(app.statusID, "right pressed")
+			app.tiles[3+rand.Intn(4)*4].image.SetFromFile(randomImage())
+		default:
+			app.statusBar.Push(app.statusID, "fam I don't know")
+		}
+	}
+
+	app.statusID = app.statusBar.GetContextId("arrow keys")
+
 	builder.ConnectSignals(signalmap)
-	fmt.Println(signalmap)
+	signalmap["resetClicked"].(func())()
 	app.window.Show()
 	gtk.Main()
+}
+
+func randomImage() string {
+	for _, v := range nums {
+		return v
+	}
+	return "error"
 }
